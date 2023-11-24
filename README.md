81c76191835726f38339885e74e30ccbd4e19ea9
Syr0 <joachimsester@web.de>
2023-11-24 22:56:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+<p align="center">
+  <img src="Logo.png" alt="R.E.L.A.X Logo">
+  <h1 align="center">R.E.L.A.X. (Regular Expression Link Analysis Xpert)
+  <br>
+  <a href="#"><img src="https://img.shields.io/badge/Supported-Absolutely-brightgreen"></a>
+  <a href="#"><img src="https://img.shields.io/badge/Version-1.3.2-blue"></a>
+  <a href="#"><img src="https://img.shields.io/badge/Bug--Free-Flawless-green"></a>
+  <a href="#"><img src="https://img.shields.io/badge/ChatGPT-Approved-red"></a>
+  <a href="#"><img src="https://img.shields.io/badge/License-MIT-orange"></a></h1>
+</p>
+
+
+Enhance your Obsidian experience with R.E.L.A.X., a powerful plugin that leverages regular expressions for dynamic and intelligent note linking.
+
+### Features That Let You Chill:
+
+- **Semiauto-Linking Magic:** Forget manual linking. Rightclick, let the regex patterns do the heavy lifting.
+- **Chillax with Organization:** Watch your notes find their buddies, automagically.
+- **Customizable Patterns:** Your rules, your regex. Tailor them to fit your groove.
+- **Smooth Obsidian Ride:** Like butter on a hot skillet, it integrates seamlessly.
+- **Fun with Regex:** Not just powerful, but also a playground for your regex skills.
+
+### Why Should You R.E.L.A.X.?
+
+Embrace a workflow that's as chilled out as a lazy Sunday afternoon. Discover connections in your notes that you didn’t even know existed. Let your creative juices flow uninterrupted while R.E.L.A.X. takes care of the mundane.
+
+### Get Started:
+
+1. **Install:** Plug it into Obsidian with zero hassles.
+2. **Set Your Regex:** Dial in your preferences.
+3. **Watch the Magic:** Update inside your selection, or the whole file or even whole folders recursively. Rightclick->There you go!
+
+## Installation:
+
+1. **Access Obsidian Settings:** Open your Obsidian application and click on the 'Settings' icon.
+2. **Enable Community Plugins:** In the Settings, navigate to 'Community Plugins' and turn off 'Restricted Mode'.
+3. **Browse and Install Plugins:** Click on 'Browse' and find the R.E.L.A.X. plugin.
+4. **Install the Plugin:** Click 'Install' on the plugin’s page.
+5. **Enable the Plugin:** Toggle it on in the 'Installed plugins' section.
+6. **Configure the Plugin (Optional):** Set up the plugin in its settings panel.
+7. **Start Using the Plugin:** Begin your seamless Obsidian experience.
+
+## Screenshots:
+<img src="Screenshot_1.bmp" alt="R.E.L.A.X Screenshot">
+<img src="Screenshot_2.bmp" alt="R.E.L.A.X Screenshot">
+<img src="Screenshot_3.bmp" alt="R.E.L.A.X Screenshot">
+
+### Community & Support:
+
+- **Forums & Discord:** Chill with fellow users on [Obsidian Forums](#) and [Discord](#).
+- **Need Help?:** Holler on our [GitHub](#).
+
+### License:
+
+R.E.L.A.X. comes with an MIT License - free to use but at your own risk. Check [LICENSE](#) for more details.
+
+### Contributions:
+
+Got ideas? Sprinkle your magic in [CONTRIBUTING.md](#).
+
+### Shoutouts:
+
+Big thanks to the Obsidian crew and all the brilliant minds behind this project.
+
+#### Support R.E.L.A.X.:
+
+Enjoying R.E.L.A.X.? [Buy me a coffee](https://buymeacoffee.com/Syr1) and keep the vibes going!
---
+++ main.ts
@@ -0,0 +1,573 @@
+import {App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, TFile} from 'obsidian';
+
+interface RelaxPluginSettings {
+	regexPairs: Array<{isActive: boolean, key: string, regex: string,  }>;
+	ignoreLinks?: boolean;
+	ignoreURLs?: boolean;
+	defangURLs?: boolean;
+}
+const DEFAULT_SETTINGS: RelaxPluginSettings = {
+	regexPairs: [],
+	ignoreLinks: true,
+	ignoreURLs: true
+};
+class Mutex {
+	private _promise: Promise<void> | null = null;
+	private _resolve: (() => void) | null = null;
+
+	async lock() {
+		if (this._promise) await this._promise;
+		this._promise = new Promise((resolve) => {
+			this._resolve = resolve;
+		});
+	}
+
+	unlock() {
+		if (this._resolve) {
+			this._resolve();
+			this._resolve = null;
+			this._promise = null;
+		}
+	}
+
+	async runExclusive<T>(callback: () => Promise<T>): Promise<T> {
+		await this.lock();
+		try {
+			return await callback();
+		} finally {
+			this.unlock();
+		}
+	}
+}
+
+class RelaxSettingTab extends PluginSettingTab {
+	plugin: RelaxPlugin;
+	keyValueContainer: HTMLDivElement;
+	constructor(app: App, plugin: RelaxPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+	display() {
+		const { containerEl } = this;
+		containerEl.empty();
+		const keyValueContainer = containerEl.createEl("div");
+		keyValueContainer.style.display = 'flex';
+		keyValueContainer.style.flexDirection = 'column';
+		function validateContent(content) {
+			const regex = /\[\[(.+?)\]\]/g;
+			return !regex.test(content);
+		}
+
+		function applyValidationStyle(textarea) {
+			if (validateContent(textarea.value)) {
+				const accentColor = getComputedStyle(document.documentElement)
+					.getPropertyValue('--color-accent').trim();
+				textarea.style.borderColor = accentColor
+			} else {
+				textarea.style.borderColor = "red";
+			}
+		}
+
+		document.addEventListener('DOMContentLoaded', (event) => {
+			const modalButton = document.querySelector("#openModalButton");
+
+			modalButton.addEventListener('click', function() {
+				const modal = document.querySelector(".modal");
+				const textarea = modal.querySelector("textarea");
+
+				applyValidationStyle(textarea);
+
+				textarea.addEventListener('input', function() {
+					applyValidationStyle(textarea);
+				});
+			});
+		});
+
+		const validateRegexInput = (input: HTMLInputElement) => {
+			let errorMsg = '';
+			try {
+				let reg = new RegExp(input.value);
+				let groupCount = (input.value.match(/\((?!\?)/g) || []).length;
+				if (groupCount > 1) {
+					input.style.border = '2px solid red';
+					errorMsg = 'More than one group detected.';
+				} else {
+					input.style.border = '';
+				}
+			} catch (e) {
+				input.style.border = '2px solid red';
+				errorMsg = 'Invalid regex.';
+			}
+
+			const errorElement = input.nextSibling;
+			if (errorElement && errorElement.classList.contains('regex-error')) {
+				errorElement.textContent = errorMsg;
+			} else {
+				const span = document.createElement('span');
+				span.className = 'regex-error';
+				span.textContent = errorMsg;
+				input.parentNode.insertBefore(span, input.nextSibling);
+			}
+		};
+		const addKeyValue = (key?: string, value?: string, isActive: boolean = false) => {
+			const row = keyValueContainer.createEl("div");
+			row.style.display = 'flex';
+			row.style.alignItems = 'center';
+
+			const dragHandle = row.createEl("span", { className: "drag-handle", text: "☰" });
+			const activeCheckbox = row.createEl("input", { className: "active-checkbox" });
+			activeCheckbox.type = "checkbox";
+			activeCheckbox.checked = isActive;
+			row.appendChild(activeCheckbox);
+
+			const keyInput = row.createEl("input", { className: "key-input", placeholder: "Description-Key", value: key ?? '' });
+			const valueInput = row.createEl("input", { className: "value-input", placeholder: "Regexp", value: value ?? '' });
+			keyInput.style.marginRight = '10px';
+			valueInput.style.marginRight = '10px';
+
+			row.createEl("button", { text: "Delete", className: `delete-button-${key ?? Date.now()}` })
+				.addEventListener("click", () => {
+					row.remove();
+					updateRegexOrderFromDOM.call(this);
+				});
+
+			if (dragHandle) makeDraggable(row, dragHandle);
+			valueInput.addEventListener('input', () => validateRegexInput(valueInput));
+		};
+
+		containerEl.createEl("button", { text: "Add Regexp" }).addEventListener("click", () => addKeyValue());
+
+		for (const {isActive, key, regex } of this.plugin.settings.regexPairs) {
+			addKeyValue(key, regex, isActive);
+		}
+
+		let startY = 0;
+		let startTop = 0;
+		let initialOffsetY = 0
+		function updateRegexOrderFromDOM(pluginObj) {
+
+			const regexPairs = [];
+			keyValueContainer.querySelectorAll("div").forEach(row => {
+
+				const activeCheckboxInput = row.querySelector('input[type="checkbox"]');
+				const keyInput = row.querySelector('input[placeholder="Description-Key"]');
+				const valueInput = row.querySelector('input[placeholder="RegExp"]');
+
+
+				if (keyInput && valueInput && activeCheckboxInput) {
+					const key = keyInput.value;
+					const value = valueInput.value;
+					const isActive = activeCheckboxInput.checked;
+					regexPairs.push({ isActive, key, regex: value });
+				}
+			});
+			if (pluginObj.plugin && pluginObj.plugin.settings) {
+				pluginObj.plugin.settings.regexPairs = regexPairs;
+				pluginObj.plugin.saveSettings();
+			} else {
+				console.error("Plugin or settings not available");
+			}
+
+		}
+		let dragElement = null;
+		let currentIndex = null;
+		let newIndex = null;
+
+
+		function makeDraggable(element, dragHandle) {
+			if (!dragHandle) {
+				console.error("Drag handle not found!", element.innerHTML);
+				return;
+			}
+
+			dragHandle.addEventListener('mousedown', function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+
+				const settingsContainerTop = document.querySelector(".vertical-tab-content-container").getBoundingClientRect().top;
+				const elementRect = element.getBoundingClientRect();
+				initialOffsetY = e.clientY - elementRect.top + settingsContainerTop;
+				currentIndex = [...element.parentElement.children].indexOf(element);
+
+				dragElement = element;
+				dragElement.style.zIndex = '1000';
+				dragElement.style.position = 'absolute';
+
+				const newTop = e.clientY - initialOffsetY;
+				dragElement.style.top = `${newTop}px`;
+
+				document.addEventListener('mousemove', onDragMove);
+				document.addEventListener('mouseup', onDragEnd);
+			});
+		}
+
+		function onDragMove(e) {
+			if (dragElement) {
+				const newTop = e.clientY - initialOffsetY;
+				dragElement.style.top = `${newTop}px`;
+
+				newIndex = null;
+				let closestDistance = Infinity;
+				[...dragElement.parentElement.children].forEach((child, idx) => {
+					if (child !== dragElement) {
+						const rect = child.getBoundingClientRect();
+						const distance = Math.abs(rect.top + rect.height / 2 - e.clientY);
+						if (distance < closestDistance) {
+							closestDistance = distance;
+							newIndex = idx;
+						}
+					}
+				});
+			}
+		}
+
+		function onDragEnd() {
+			if (dragElement) {
+				dragElement.style.position = '';
+				dragElement.style.top = '';
+				dragElement.style.zIndex = '';
+
+				if (newIndex !== null && currentIndex !== null && newIndex !== currentIndex) {
+					const parent = dragElement.parentElement;
+					if (newIndex >= parent.children.length) {
+						parent.appendChild(dragElement);
+					} else {
+						parent.insertBefore(dragElement, parent.children[newIndex + (newIndex > currentIndex ? 1 : 0)]);
+					}
+				}
+
+				dragElement = null;
+				updateRegexOrderFromDOM.call(this);
+				document.removeEventListener('mousemove', onDragMove);
+				document.removeEventListener('mouseup', onDragEnd);
+				reorderElements();
+				currentIndex = null;
+				newIndex = null;
+			}
+		}
+
+		function reorderElements() {
+			keyValueContainer.querySelectorAll("div").forEach((row) => {
+				row.style.top = '';
+			});
+		}
+
+		new Setting(containerEl)
+			.setName('Ignore Links')
+			.addToggle(toggle => {
+				toggle
+					.setValue(this.plugin.settings.ignoreLinks ?? true)
+					.onChange(async value => {
+						this.plugin.settings.ignoreLinks = value;
+						await this.plugin.saveSettings();
+					});
+			});
+		new Setting(containerEl)
+			.setName('Ignore URLs')
+			.addToggle(toggle => {
+				toggle
+					.setValue(this.plugin.settings.ignoreURLs ?? true)
+					.onChange(async value => {
+						this.plugin.settings.ignoreURLs = value;
+						await this.plugin.saveSettings();
+					});
+			});
+		new Setting(containerEl)
+			.setName('Defang URLs')
+			.addToggle(toggle => {
+				toggle
+					.setValue(this.plugin.settings.defangURLs ?? true)
+					.onChange(async value => {
+						this.plugin.settings.defangURLs = value;
+						await this.plugin.saveSettings();
+					});
+			});
+		new Setting(containerEl)
+			.setName("Save")
+			.addButton(button => {
+				button.setButtonText("Save")
+					.onClick(async () => {
+						updateRegexOrderFromDOM(this)
+						const closeButton = document.querySelector('.modal-close-button');
+						if (closeButton) {
+							closeButton.click();
+						}
+					});
+			});
+	}
+}
+function containsValidLink(line: string, match: string): boolean {
+	const linkRegex = /\[\[.*?\]\]/g;
+	let result;
+	while ((result = linkRegex.exec(line)) !== null) {
+		if (result.index <= line.indexOf(match) && linkRegex.lastIndex >= line.indexOf(match) + match.length) {
+			return true;
+		}
+	}
+	return false;
+}
+
+export default class RelaxPlugin extends Plugin {
+	settings: RelaxPluginSettings;
+	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new RelaxSettingTab(this.app, this));
+		this.addCommand({id: 'Relax-command', name: 'Relax', callback: () => this.addBrackets()});
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("R.E.L.A.X.")
+						.setIcon("curly-braces")
+						.onClick(async () => {
+							this.addBrackets();
+						});
+				});
+			})
+		);
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("R.E.L.A.X.")
+						.setIcon("curly-braces")
+						.onClick(async () => {
+							this.addBrackets();
+						});
+				});
+			})
+		);
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					item
+						.setTitle("Remove all brackets")
+						.setIcon("curly-braces")
+						.onClick(async () => {
+							this.removeBrackets();
+						});
+				});
+			})
+		);
+	}
+	onunload() {
+	}
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+	async saveSettings() {
+		await this.saveData(this.settings);
+		new Notice('Config saved!');
+	}
+
+	removeBracketsInSelection(content: string): string {
+		return content.replace(/\[|\]/g, '');
+	}
+
+	async addBracketsForFile(noteFilePath = "") {
+		await this.processFileContent(noteFilePath, (content) => this.updateSelection(content, this.settings));
+	}
+
+	async removeBracketsinFile(noteFilePath = "") {
+		await this.processFileContent(noteFilePath, this.removeBracketsInSelection);
+	}
+
+	async processFileContent(noteFilePath: string, contentProcessor: (content: string) => string): Promise<void> {
+		let noteFile;
+
+		let fileContent = "";
+
+		if (noteFilePath !== "") {
+			noteFile = this.app.vault.getAbstractFileByPath(noteFilePath);
+			if (!noteFile) {
+				new Notice(`No file found at the given path: ${noteFilePath}`);
+				return;
+			}
+			fileContent = await this.app.vault.read(noteFile as TFile);
+		} else {
+			const leaf = this.app.workspace.activeLeaf || this.app.workspace.getLeaf();
+			noteFile = this.app.workspace.getActiveFile();
+
+			if (!noteFile && leaf.view.focusedItem && !leaf.view.focusedItem.collapsible) {
+				noteFile = leaf.view.focusedItem.file;
+			}
+
+			if (!noteFile) {
+				new Notice('No file selected. Please select a markdown file from the editor or navigation bar.');
+				return;
+			}
+
+			if (!(leaf.view instanceof MarkdownView)) {
+				new Notice('Please open a markdown file or select a folder');
+				return;
+			}
+
+			fileContent = await this.app.vault.read(noteFile as TFile);
+		}
+
+		const updatedContent = contentProcessor(fileContent);
+		await this.app.vault.modify(noteFile as TFile, updatedContent);
+	}
+
+	updateSelection(content: string, settings: RelaxPluginSettings): string {
+		const urlRegex = /(https?:\/\/[^\s]+)/g;
+		const excludedExtensions = /\.(exe|lnk|xls|md|sh|elf|bin|tmp|doc|odt|docx|pdf|yara|dll|txt)$/;
+		const fangMap = {
+			'[.]': '.',
+			'[:]': ':'
+		};
+		if (settings.defangURLs) {
+			content = content.replace(/\[\.\]|\[\:\]/g, char => fangMap[char]);
+		}
+
+		let updatedText = '';
+		const lines = content.split("\n");
+
+		lines.forEach((line, index) => {
+			for (const { isActive,key, regex } of settings.regexPairs) {
+				if (!isActive) {
+					continue;
+				}
+
+				const compiledRegex = new RegExp(regex, 'g');
+				line = line.replace(compiledRegex, (match, ...args) => {
+					const groups = args.slice(0, -2).filter(g => g !== undefined);
+					const capturedValue = groups[0];
+
+					if (!capturedValue) return match;
+
+					if (settings.ignoreLinks && containsValidLink(line, capturedValue)) {
+						return match;
+					}
+
+					if (settings.ignoreURLs) {
+						const urls = Array.from(line.matchAll(urlRegex), m => m[0]);
+						let ignoreCurrentMatch = false;
+
+						for (const url of urls) {
+							if (!excludedExtensions.test(url) && url.includes(capturedValue)) {
+								ignoreCurrentMatch = true;
+								break;
+							}
+						}
+
+						if (ignoreCurrentMatch) {
+							return match;
+						}
+					}
+
+					return match.replace(capturedValue, `[[${capturedValue}]]`);
+				});
+			}
+			updatedText += line;
+
+			if (index !== lines.length - 1) {
+				updatedText += "\n";
+			}
+		});
+
+		return updatedText;
+	}
+
+	async processMarkdownContent(action: "removeBrackets" | "addBrackets") {
+		const activeLeaf = this.app.workspace.activeLeaf;
+
+		if (!activeLeaf) {
+			new Notice('Please open a markdown file or select a folder');
+			return;
+		}
+
+		const view = activeLeaf.view;
+		if (!view) {
+			new Notice('Unknown item selected. Please select a markdown file or folder');
+			return;
+		}
+
+		if (view instanceof MarkdownView) {
+			const selection = view.editor.getSelection();
+
+			if (selection && selection.trim().length !== 0) {
+				let updatedSelection;
+				if (action === 'removeBrackets') {
+					updatedSelection = this.removeBracketsInSelection(selection);
+					new Notice('Removed brackets from selection!');
+				} else {
+					updatedSelection = this.updateSelection(selection, this.settings);
+					new Notice('Added brackets in selection!');
+				}
+				view.editor.replaceSelection(updatedSelection);
+				new Notice(action === 'removeBrackets' ? 'Removed brackets from selection!' : 'Updated content in selection!');
+			} else {
+				if (action === 'removeBrackets') {
+					await this.removeBracketsinFile();
+					new Notice('Removed brackets from entire file!');
+				} else { 
+					await this.addBracketsForFile();
+					new Notice('Added brackets on entire file!');
+				}
+			}
+		}
+	}
+
+	async removeBrackets() {
+		await this.processMarkdownContent('removeBrackets');
+	}
+
+	async addBrackets() {
+		const activeLeaf = this.app.workspace.activeLeaf;
+
+		if (!activeLeaf) {
+			new Notice('Please open a markdown file or select a folder');
+			return;
+		}
+
+		const view = activeLeaf.view;
+		if (!view) {
+			new Notice('Unknown item selected. Please select a markdown file or folder');
+			return;
+		}
+
+		if (view instanceof MarkdownView) {
+			const editor = view.editor;
+			const selection = editor.getSelection();
+
+			if (selection && selection.trim().length !== 0) {
+				const updatedSelection = this.updateSelection(selection, this.settings);
+				editor.replaceSelection(updatedSelection);
+				new Notice('Added brackets in selection!');
+			} else {
+				await this.addBracketsForFile();
+				new Notice('Updated entire file!');
+			}
+		} else if (view.focusedItem && view.focusedItem.collapsible) {
+			const folderPath = view.focusedItem.file.path;
+			await this.addBracketsForFolder(folderPath);
+		} else if (view.focusedItem && !view.focusedItem.collapsible) {
+			const filePath = view.focusedItem.file.path;
+			await this.addBracketsForFile(filePath);
+		} else {
+			new Notice('No markdown file or folder is currently selected. Please select one.');
+		}
+	}
+
+
+	private progress = 0;
+	private mutex = new Mutex();
+
+	async addBracketsForFolder(folderPath: string) {
+		const files = this.app.vault.getMarkdownFiles().filter(file => file.path.startsWith(folderPath));
+		const totalFiles = files.length;
+
+		let processingNotice = new Notice(`Processing ${totalFiles} files...`,totalFiles*1000);
+
+		for (const [index, file] of files.entries()) {
+			await this.mutex.runExclusive(async () => {
+				await this.addBracketsForFile(file.path);
+				processingNotice.setMessage(`Processing file ${index + 1} of ${totalFiles}`);
+			});
+		}
+
+		processingNotice.hide();
+		new Notice(`All ${totalFiles} files in the folder processed.`);
+	}
+}
